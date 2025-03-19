86f990cbfe73ed1f9263107ec0824ac7a5d1ad20
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2025-03-19 08:58:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,8 +88,8 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'fileclone12') # Public
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+ipiam4tiR4E0ZTE1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Ellisa_movies_hub')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/CLMovieRequestGrp')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/CinemaChannelNewz')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ellisa_support_group') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Vijayofficial77')
 
