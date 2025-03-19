6ee177e4b6cc222d722f9f0f3b1362b011d0e8a1
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2025-03-19 09:00:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://envs.sh/1Mf.jpg https://envs.sh/Rqv.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/nFO.jpg https://envs.sh/nez.jpg')).split()
 
 
 # Admins & Users
